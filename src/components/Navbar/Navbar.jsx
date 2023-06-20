e3880d1305b2146c8834e0cf26343138117b9f9d
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-06-20 12:15:04 +0200

    Navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { AiOutlineShopping } from 'react-icons/ai';
+import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+      <nav>
+        <div className="logo">
+          <NavLink to='/' className="logo-link">Nav</NavLink>
+        </div>
+
+        <ul>
+          <li>
+            <NavLink to='/profile' className="profile">Profile</NavLink></li>
+          <li>Log in</li>
+          <li>
+              <span className='cart-number'>0</span>
+            <AiOutlineShopping className='cart-logo' />
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
